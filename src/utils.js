58511e47d56fc3dc0150a42af362278068d8d215
MJ.Kim <minjini57@gmail.com>
2020-10-07 16:20:27 -0700

    sendMail Function with Nodemailer & Mailgun

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mg from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.API_KEY,
+      domain: process.env.DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mg(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "xxxxxx@naver.com",
+    to: address,
+    subject: "Login Secret for CloneInstagram",
+    html: `Hello! Your login secret is ${secret}.<br/>Copy pasted on the app/website to log in`,
+  };
+  return sendMail(email);
 };
